26d8aabe4a10b555d92ebd8ad1199e635d3950d4
Onur Er <onurer@gmail.com>
2018-03-10 08:40:43 -0500

    * Edited assembly info and settings.

--- BindableProperties.Test/Properties/AssemblyInfo.cs
+++ BindableProperties.Test/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("nunit.tests")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
