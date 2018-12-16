b2ae44b208fe50b24f97e98b42260660202905bb
Onur Er <onurer@gmail.com>
2018-12-15 20:52:57 -0800

    BP-11: Implemented YourClass of monitoring example.

---
+++ Examples/5_InheritedPropertySystemExampleViaMonitoringSystem/Skin.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Media;
+using Atesh.BindableProperties;
+
+namespace InheritedPropertySystemExampleViaMonitoringSystem
+{
+    class Skin
+    {
+        //todo: BackgroundColor property doesn't need to be a bindable property but we need it as a monitoring target until regular property targeting support gets implemented.
+        public readonly PrivatelyBindablePropertyWithEmptyValue<Color> BackgroundColor;
+
+        public bool BackgroundColorIsEmpty { get; private set; }
+
+        public Skin()
+        {
+            BackgroundColor = new PrivatelyBindablePropertyWithEmptyValue<Color>(this, out _, true);
+
+            BackgroundColor.Changed += BackgroundColor_Changed;
+        }
+
+        void BackgroundColor_Changed(PrivatelySettablePrivatelyBindableProperty<Color> Sender, ChangedEventArgs<Color> Args) => BackgroundColorIsEmpty = Args.IsEmpty;
+    }
+}
