3559f48d36d248a1263045fb328862a477b89f52
Onur Er <onurer@gmail.com>
2019-11-09 09:17:33 -0800

    * Comments are updated.

--- Examples/5_InheritedPropertySystemExampleViaMonitoringSystem/Skin.cs
+++ Examples/5_InheritedPropertySystemExampleViaMonitoringSystem/Skin.cs
@@ -5,7 +5,6 @@ namespace InheritedPropertySystemExampleViaMonitoringSystem
 {
     public class Skin
     {
-        //todo: BackgroundColor property doesn't need to be a bindable property but we need it as a monitoring target until regular property targeting support gets implemented.
         public readonly PrivatelyBindablePropertyWithEmptyValue<Color> BackgroundColor;
 
         public bool BackgroundColorIsEmpty { get; private set; }
